521a37d2a6693f11624dfd4cd724d00864546250
raha <slrrraha@gmail.com>
2022-06-30 14:42:48 +0430

    q1Done

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".btn").click(function(fade){
+        $(".crd3").fadeToggle(1000);
+        $(".crd2").fadeToggle(3000);
+        $(".crd1").fadeToggle(5000);
+    });
+});
